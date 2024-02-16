8cb6c5a4e46e24d028db305f1c210e6f5eba46d6
karimullam7 <karimullamahammadsvn@gmail.com>
2024-02-16 12:36:25 +0530

    ind vs aut score

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Todo from './counter';
+
+import Counter from './counter';
 
 function App() {
   return (
     <div className="App">
-      <Todo/>
+<Counter cname="India" cstart={5000} ad ={1000} ></Counter>
+<Counter cname="Australia" cstart={200} ad={5}></Counter>
+
     </div>
   );
 }

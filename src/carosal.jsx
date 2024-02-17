deafab7ffe23a0ed276dbb5695060d7e69bc67a3
karimullam7 <karimullamahammadsvn@gmail.com>
2024-02-17 15:57:44 +0530

    EdupolyStudents

--- src/carosal.jsx
+++ src/carosal.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
 function Imageslide() {
+  var [CurrentI, setCurrentI] = React.useState(0);
+
   const reviews = [
     {
       id: 1,
-      name: "Prbhas Raju",
+      name: "Prabhas Raju",
       job: "The Boss",
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU7OcFQzYmBT8JTx3EcCmWg1BmH1AZm3fSo59WjKDmRfrp8mPEio7mRishPr1Y9qnJUYw&usqp=CAU",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_z5nd2nGsk16tj6SeXzGGsmrmIn0AyGHUFQ&usqp=CAU",
       text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
       id: 2,
       name: "Hrithik Babu",
       job: "web designer",
-      image: "https://pbs.twimg.com/media/GEXj084bwAAbG9l.jpg",
+      image: "https://goldandhra.com/wp-content/uploads/2024/01/Hrithik-Roshan-Next-Act-Exploring-Situational-Comedy.jpg",
       text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
     },
     {
@@ -35,26 +37,41 @@ function Imageslide() {
     },
   ];
 
+  function PrevRvw() {
+    CurrentI === 0
+      ? setCurrentI(CurrentI + (reviews.length - 1))
+      : setCurrentI(CurrentI - 1);
+  }
+  function NextvRvw() {
+    CurrentI === reviews.length - 1
+      ? setCurrentI(CurrentI - (reviews.length - 1))
+      : setCurrentI(CurrentI + 1);
+  }
+
   return (
     <div className="main">
       <div className="sub1">
         <h1>Edupoly Students</h1>
       </div>
+      <div className="li">
+        <li>
+          <img className="img" src={reviews[CurrentI].image}></img>
+        </li>
+        <li className="a">{reviews[CurrentI].name}</li>
+        <li className="b">{reviews[CurrentI].job}</li>
+        <li className="c">{reviews[CurrentI].text}</li>
+        <div className="spb">
+          <i className="bia bi-arrow-left-circle" onClick={PrevRvw}></i>
+          <i className="bib bi-arrow-right-circle" onClick={NextvRvw}></i>
+        </div>
+        <div>
+          <i className="btn btn-primary">Suprice Me</i>
+        </div>
 
+      </div>
       <div className="sub">
-        {reviews.map((a) => {
-          return (
-            <ul className="ul">
-              <li className="name">{a.name}</li>
-              <li className="name1">{a.job}</li>
-              <li>{<img className="img" src={a.image}></img>}</li>
-              <li>{a.text}</li>
-              <li class="bia bi-arrow-left-circle"></li>
-              <li class="bib bi-arrow-right-circle"></li>
-              <li className="btn btn-primary">Suprice Me</li>
-            </ul>
-          );
-        })}
+        <div></div>
+        
       </div>
     </div>
   );

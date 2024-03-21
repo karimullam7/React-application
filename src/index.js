cffd1ab47ead45e3d053b687619ddf182024a480
karimullam7 <karimullamahammadsvn@gmail.com>
2024-03-21 16:29:07 +0530

    ka

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+
+ReactDOM.render(
+    <Router>
+        <App/>
+    </Router>,
+    document.getElementById("root")
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ 
     <App />
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Rooms.js
@@ -0,0 +1,12 @@
+import React from 'react' ;
+
+function Rooms() {
+  return (
+    <div>
+      Hello from Room page
+      
+    </div>
+  )
+}
+
+export default Rooms

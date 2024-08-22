645907d8b29dfa78e4eef0876ea6e545d26d4c54
karimullam7 <karimullamahammadsvn@gmail.com>
2024-08-22 18:25:32 +0530

    K1

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,14 +3,13 @@ import Singin from './Singin'
 import Sinout from './Sinout'
 
 function Header({hide}) {
+  console.log(hide);
 
-
-    
   return (
     <div className='head'>
-      <h1>This is hEADER</h1>
-       {/* { hide ?  <Singin/> : <Sinout/>}   */}
-       { !hide ? (<Singin/>) : <Sinout/> }
+      <h1>This is hEADER</h1>     
+       { hide ?  <Singin/> : <Sinout/>}  
+
 
     </div>
   )
